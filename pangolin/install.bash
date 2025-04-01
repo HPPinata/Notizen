dbaebba1a96680356d7fd8a40402784ee52b3d03
HPPinata <83947761+HPPinata@users.noreply.github.com>
2025-04-01 14:53:51 +0200

    minor fixes

--- pangolin/install.bash
+++ pangolin/install.bash
@@ -4,12 +4,13 @@ mkdir /var/pangolin
 cd /var/pangolin
 
 apt install -y wget
-wget -O dyndns.bash https://raw.githubusercontent.com/HPPinata/Notizen/refs/heads/main/pangolin/dyndns.bash && chmod +x ./dyndns.bash
+wget -O dyndns.bash "https://raw.githubusercontent.com/HPPinata/Notizen/refs/heads/main/pangolin/dyndns.bash" && chmod +x ./dyndns.bash
 wget -O installer "https://github.com/fosrl/pangolin/releases/latest/download/installer_linux_$(uname -m | sed 's/x86_64/amd64/;s/aarch64/arm64/')" && chmod +x ./installer
 
 read -p "DynDNS Domain: " ZONE
 read -p "Auth Token: " TK
 
+apt install -y dnsutils
 ./dyndns.bash
 ./installer
 
